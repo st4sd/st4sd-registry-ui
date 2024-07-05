9886c7090f40fe309651ce5cbfc549da131958c8
Alexander Peverett <Alexander.Peverett-CIC@ibm.com>
2024-07-05 13:52:26 +0100

    refactor(St4sdHeader): use CDS 2.8 components (#674)
    Signed-off-by: Alexander Peverett <Alexander.Peverett-CIC@ibm.com>

--- src/components/St4sdHeader/St4sdHeader.vue
+++ src/components/St4sdHeader/St4sdHeader.vue
@@ -5,17 +5,20 @@
   Author: Alessandro Pomponio
 -->
 <template>
-  <bx-header class="position-reset" aria-label="ST4SD Registry">
-    <bx-header-name :href="getDeploymentEndpoint()" prefix="IBM"
-      >ST4SD Registry</bx-header-name
+  <cds-header
+    class="position-reset cds-theme-zone-g100"
+    aria-label="ST4SD Registry"
+  >
+    <cds-header-name :href="getDeploymentEndpoint()" prefix="IBM"
+      >ST4SD Registry</cds-header-name
     >
-    <bx-header-nav-item
+    <cds-header-nav-item
       :href="`${getDeploymentEndpoint()}build-canvas`"
       v-if="isBuildCanvasEnabled"
       class="hidden-on-mobile"
-      >Build an experiment</bx-header-nav-item
+      >Build an experiment</cds-header-nav-item
     >
-    <bx-link
+    <cds-link
       @click="openModal('login')"
       v-if="!isGlobalRegistry"
       class="hidden-on-mobile"
@@ -27,14 +30,14 @@
         width="18"
         src="@/assets/terminal.svg"
       />
-    </bx-link>
-  </bx-header>
+    </cds-link>
+  </cds-header>
   <St4sdLoginModal id="login" :url="this.url" :contextName="this.contextName" />
 </template>
 
 <script>
-import "@carbon/web-components/es/components/ui-shell/index.js";
-import "@carbon/web-components/es/components/list/index.js";
+import "https://1.www.s81c.com/common/carbon/web-components/version/v2.8.0/ui-shell.min.js";
+import "https://1.www.s81c.com/common/carbon/web-components/version/v2.8.0/link.min.js";
 import { getDeploymentEndpoint } from "@/functions/public_path";
 import St4sdLoginModal from "@/components/St4sdHeader/St4sdLoginModal.vue";
 import { registryUISharedState } from "@/stores/registryUISharedState";
@@ -75,12 +78,12 @@ export default {
   position: unset;
 }
 
-bx-link {
+cds-link {
   margin: 0 0 0 auto;
   padding: 0px 1rem 0px 2rem;
 }
 
-bx-link::part(link) {
+cds-link::part(link) {
   color: rgb(244, 244, 244);
   cursor: pointer;
   width: 120%;
