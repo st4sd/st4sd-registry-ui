c845d61b732746e152a27e2c48e37c28964f98d3
Alexander Peverett <Alexander.Peverett-CIC@ibm.com>
2024-07-10 13:31:03 +0100

    refactor(St4sdLoginModal): use CDS 2.8 components (#708)
    Signed-off-by: Alexander Peverett <Alexander.Peverett-CIC@ibm.com>
    Signed-off-by: Alessandro Pomponio <alessandro.pomponio1@ibm.com>
    Co-authored-by: Alessandro Pomponio <alessandro.pomponio1@ibm.com>

--- src/components/St4sdHeader/St4sdLoginModal.vue
+++ src/components/St4sdHeader/St4sdLoginModal.vue
@@ -1,12 +1,12 @@
 <template>
-  <bx-modal size="sm" class="no-transform">
-    <bx-modal-header>
-      <bx-modal-close-button></bx-modal-close-button>
-      <bx-modal-heading
-        >Login on the <code>stp</code> CLI tool</bx-modal-heading
+  <cds-modal size="sm">
+    <cds-modal-header>
+      <cds-modal-close-button></cds-modal-close-button>
+      <cds-modal-heading
+        >Login on the <code>stp</code> CLI tool</cds-modal-heading
       >
-    </bx-modal-header>
-    <bx-modal-body>
+    </cds-modal-header>
+    <cds-modal-body>
       <div data-modal-primary-focus>
         Copy this code and paste it in your terminal to log in to this ST4SD
         instance on the <code>stp</code> CLI.
@@ -15,21 +15,28 @@
         You will be given instructions on how to retrieve a token to complete
         the log in procedure.
       </div>
-      <bx-code-snippet>
-        stp login --context-name {{ contextName }} {{ url }}
-      </bx-code-snippet>
-    </bx-modal-body>
-    <bx-modal-footer>
-      <bx-modal-footer-button class="focus" kind="primary" data-modal-close>
+      <br />
+      <div>
+        <cds-code-snippet
+          @mouseover="alignCopyTooltipBottomRight"
+          id="stp-code-snippet"
+          class="cds-theme-zone-white"
+        >
+          stp login --context-name {{ contextName }} {{ url }}
+        </cds-code-snippet>
+      </div>
+    </cds-modal-body>
+    <cds-modal-footer>
+      <cds-modal-footer-button kind="primary" data-modal-close>
         Close
-      </bx-modal-footer-button>
-    </bx-modal-footer>
-  </bx-modal>
+      </cds-modal-footer-button>
+    </cds-modal-footer>
+  </cds-modal>
 </template>
 
 <script>
-import "@carbon/web-components/es/components/tooltip/index.js";
-import "@carbon/web-components/es/components/code-snippet/index.js";
+import "https://1.www.s81c.com/common/carbon/web-components/version/v2.8.0/code-snippet.min.js";
+import "https://1.www.s81c.com/common/carbon/web-components/version/v2.8.0/modal.min.js";
 
 export default {
   name: "St4sdLoginModal",
@@ -37,13 +44,22 @@ export default {
     url: String,
     contextName: String,
   },
+  methods: {
+    alignCopyTooltipBottomRight: () => {
+      document
+        .getElementById("stp-code-snippet")
+        .shadowRoot.querySelector("cds-copy-button")
+        .shadowRoot.querySelector("cds-copy")
+        .setAttribute("align", "bottom-right");
+    },
+  },
 };
 </script>
 
 <style lang="css" scoped>
-@import "@/styles/bx-modal-styles.css";
-
-bx-code-snippet {
-  margin-top: 0.5rem;
+/* AP: Needed because code snippet otherwise does not respect
+the dimensions of the div in which it's contained */
+cds-code-snippet {
+  box-sizing: border-box;
 }
 </style>
