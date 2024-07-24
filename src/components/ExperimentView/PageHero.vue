eb6abee1b8b5c396416952a2cf10c8133e3bb2ba
Alexander Peverett <Alexander.Peverett-CIC@ibm.com>
2024-07-24 10:09:38 +0100

    refactor(PageHero): use CDS 2.8 components  (#744)
    Signed-off-by: Alexander Peverett <Alexander.Peverett-CIC@ibm.com>
    Signed-off-by: Alessandro Pomponio <alessandro.pomponio1@ibm.com>
    Co-authored-by: Alessandro Pomponio <alessandro.pomponio1@ibm.com>

--- src/components/ExperimentView/PageHero.vue
+++ src/components/ExperimentView/PageHero.vue
@@ -1,17 +1,17 @@
 <template>
   <!-- Experiment header -->
-  <div>
-    <div class="cds--row topPad">
-      <!-- Left column  -->
-      <div class="cds--col-sm-4 cds--col-md-8 cds--col-lg-12">
-        <!-- Experiment name and description -->
-        <dds-content-block>
-          <dds-content-block-heading>{{
+  <div class="cds--row">
+    <!-- Left column  -->
+    <div class="cds--col-sm-4 cds--col-md-8 cds--col-lg-12">
+      <!-- Experiment name and description -->
+      <div>
+        <c4d-content-block>
+          <c4d-content-block-heading>{{
             experiment.metadata.package.name
-          }}</dds-content-block-heading>
-          <dds-content-block-copy>{{
-            experiment.metadata.package.description
-          }}</dds-content-block-copy>
+          }}</c4d-content-block-heading>
+          <c4d-content-block-copy>
+            {{ experiment.metadata.package.description }}
+          </c4d-content-block-copy>
 
           <!-- Experiment tags -->
           <div class="tag-group-container">
@@ -42,130 +42,125 @@
           </div>
 
           <!-- eslint-disable -->
-          <dds-cta-block no-border>
-            <dds-link-list slot="link-list" type="end">
-              <dds-link-list-item
-                v-if="!isGlobalRegistry"
-                :href="`${getDeploymentEndpoint()}experiment/${id}/runs`"
-              >
-                View Runs
-                <img
-                  slot="icon"
-                  src="@/assets/arrow--right.svg"
-                  width="20"
-                  height="20"
-                />
-              </dds-link-list-item>
-              <dds-link-list-item
-                v-if="!isCanvasDisabled"
-                :href="`${getDeploymentEndpoint()}experiment/${id}/viewcanvas`"
-              >
-                View Graph
-                <img
-                  slot="icon"
-                  src="@/assets/arrow--right.svg"
-                  width="20"
-                  height="20"
-                />
-              </dds-link-list-item>
-              <dds-link-list-item
-                :href="`${getDeploymentEndpoint()}experiment/${id}/parameterisation-options`"
-                v-if="isParameterisationEnabled"
-              >
-                Parameterisation Options
-                <img
-                  slot="icon"
-                  src="@/assets/arrow--right.svg"
-                  width="20"
-                  height="20"
-                />
-              </dds-link-list-item>
-              <dds-link-list-item
-                v-if="isRunExperimentEnabled"
-                @click="toggleModalVisibility('runExperimentFormModal')"
-              >
-                Run Experiment
-                <img
-                  slot="icon"
-                  src="@/assets/arrow--right.svg"
-                  width="20"
-                  height="20"
-                />
-              </dds-link-list-item>
-              <dds-link-list-item
-                v-if="isBuildCanvasEditingEnabled"
-                :href="`${getDeploymentEndpoint()}experiment/${this.id}/edit`"
-                :disabled="
-                  !experiment.metadata.package.keywords.includes(
+          <c4d-link-list type="end">
+            <c4d-link-list-item
+              v-if="!isGlobalRegistry"
+              :href="`${getDeploymentEndpoint()}experiment/${id}/runs`"
+            >
+              View Runs
+              <img
+                slot="icon"
+                src="@/assets/arrow--right.svg"
+                width="20"
+                height="20"
+              />
+            </c4d-link-list-item>
+            <c4d-link-list-item
+              v-if="!isCanvasDisabled"
+              :href="`${getDeploymentEndpoint()}experiment/${id}/viewcanvas`"
+            >
+              View Graph
+              <img
+                slot="icon"
+                src="@/assets/arrow--right.svg"
+                width="20"
+                height="20"
+              />
+            </c4d-link-list-item>
+            <c4d-link-list-item
+              :href="`${getDeploymentEndpoint()}experiment/${id}/parameterisation-options`"
+              v-if="isParameterisationEnabled"
+            >
+              Parameterisation Options
+              <img
+                slot="icon"
+                src="@/assets/arrow--right.svg"
+                width="20"
+                height="20"
+              />
+            </c4d-link-list-item>
+            <c4d-link-list-item
+              v-if="isRunExperimentEnabled"
+              @click="toggleModalVisibility('runExperimentFormModal')"
+            >
+              Run Experiment
+              <img
+                slot="icon"
+                src="@/assets/arrow--right.svg"
+                width="20"
+                height="20"
+              />
+            </c4d-link-list-item>
+            <c4d-link-list-item
+              v-if="isBuildCanvasEditingEnabled"
+              :href="`${getDeploymentEndpoint()}experiment/${this.id}/edit`"
+              :disabled="
+                !experiment.metadata.package.keywords.includes(
+                  'internal-experiment',
+                )
+              "
+              :title="
+                !experiment.metadata.package.keywords.includes(
+                  'internal-experiment',
+                )
+                  ? 'This experiment was created outside the canvas and therefore it cannot be edited'
+                  : ''
+              "
+            >
+              Edit Experiment
+              <img
+                v-if="
+                  experiment.metadata.package.keywords.includes(
                     'internal-experiment',
                   )
                 "
-                :title="
-                  !experiment.metadata.package.keywords.includes(
-                    'internal-experiment',
-                  )
-                    ? 'This experiment was created outside the canvas and therefore it cannot be edited'
-                    : ''
-                "
-              >
-                Edit Experiment
-                <img
-                  v-if="
-                    experiment.metadata.package.keywords.includes(
-                      'internal-experiment',
-                    )
-                  "
-                  slot="icon"
-                  src="@/assets/arrow--right.svg"
-                  width="20"
-                  height="20"
-                />
-              </dds-link-list-item>
-            </dds-link-list>
-          </dds-cta-block>
-          <!-- eslint-enable -->
-        </dds-content-block>
+                slot="icon"
+                src="@/assets/arrow--right.svg"
+                width="20"
+                height="20"
+              />
+            </c4d-link-list-item>
+          </c4d-link-list>
+        </c4d-content-block>
+        <!-- eslint-enable -->
       </div>
-
-      <!-- Right column -->
-      <div class="cds--col-sm-4 cds--col-md-8 cds--col-lg-4 pad1">
-        <!-- Progress indicator -->
-        <St4sdBestPracticesProgressIndicator
-          :loading="loading"
-          :experiment="experiment"
-        />
-      </div>
-      <runExperimentFormModal
-        v-if="runExperimentFormModal"
-        @submit="toggleRunExperimentFormEmit"
-        @bx-modal-closed="toggleModalVisibility('runExperimentFormModal')"
-        open="true"
-        title="Run Experiment"
-      >
-        <runExperimentForm
-          :formEmit="runExperimentFormEmit"
-          @post-experiment-run="setRunExperimentFormPayload"
-        />
-      </runExperimentFormModal>
     </div>
+
+    <!-- Right column -->
+    <div class="cds--col-sm-4 cds--col-md-8 cds--col-lg-4 pad1">
+      <!-- Progress indicator -->
+      <St4sdBestPracticesProgressIndicator
+        :loading="loading"
+        :experiment="experiment"
+      />
+    </div>
+    <runExperimentFormModal
+      v-if="runExperimentFormModal"
+      @submit="toggleRunExperimentFormEmit"
+      @bx-modal-closed="toggleModalVisibility('runExperimentFormModal')"
+      open="true"
+      title="Run Experiment"
+    >
+      <runExperimentForm
+        :formEmit="runExperimentFormEmit"
+        @post-experiment-run="setRunExperimentFormPayload"
+      />
+    </runExperimentFormModal>
   </div>
 </template>
 
 <script>
-import { checkVeInterfaceIsPresent } from "@/functions/ve_interface";
-import { getDeploymentEndpoint } from "@/functions/public_path";
+import "https://1.www.s81c.com/common/carbon-for-ibm-dotcom/version/v2.8.0/content-block.min.js";
+import "https://1.www.s81c.com/common/carbon/web-components/version/v2.8.0/tag.min.js";
+import "https://1.www.s81c.com/common/carbon-for-ibm-dotcom/version/v2.8.0/link-list.min.js";
 
 import St4sdBestPracticesProgressIndicator from "@/components/St4sdBestPracticesProgressIndicator";
 import runExperimentFormModal from "@/canvas/components/modals/experiment/runExperimentFormModal.vue";
 import runExperimentForm from "@/canvas/components/forms/runExperimentForm.vue";
 
-import "@carbon/ibmdotcom-web-components/es/components/link-list/index.js";
-import "@carbon/ibmdotcom-web-components/es/components/cta-section/index.js";
-import "@carbon/web-components/es/components/accordion/index.js";
-import "@carbon/web-components/es/components/textarea/index.js";
-import "@carbon/web-components/es/components/tabs/index.js";
-import "@carbon/web-components/es/components/toggle/index.js";
-import "https://1.www.s81c.com/common/carbon/web-components/version/v2.8.0/tag.min.js";
+import { checkVeInterfaceIsPresent } from "@/functions/ve_interface";
+import { getDeploymentEndpoint } from "@/functions/public_path";
+
 import axios from "axios";
 import router from "@/router";
 
@@ -258,63 +253,27 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
+@use "@carbon/layout";
 @import "@/styles/cds-tag-styles.scss";
 
-dds-content-block {
-  padding-bottom: 0;
-  padding-top: 0;
-}
-cds-structured-list-cell {
-  overflow-wrap: anywhere;
-}
-
-dds-cta-block {
-  padding: 0;
-  font-size: 16px;
-
-  @media screen and (max-width: 671px) {
-    padding-top: 2rem;
-  }
-}
-
-dds-content-block-copy {
+c4d-content-block {
   margin: 0;
 }
 
-bx-toggle {
-  padding-bottom: 1rem !important;
+c4d-content-block-heading,
+c4d-content-block-copy {
+  margin-left: 0;
 }
 
-.inputs {
-  width: 100%;
+c4d-cta-block {
+  display: inline;
 }
 
-.input-wrapper {
-  width: 90%;
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
-  align-items: center;
-}
+c4d-link-list {
+  margin-top: layout.$spacing-07;
 
-.remove-button {
-  width: 7.5% !important;
-  background-color: #0f62fe;
-}
-
-.add-button {
-  width: 12% !important;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-</style>
-<style scoped lang="scss">
-@media screen and (max-width: 671px) {
-  dds-content-block-copy,
-  dds-content-block-heading,
-  dds-text-cta {
+  @media screen and (max-width: 671px) {
     margin-left: 0;
   }
 }
