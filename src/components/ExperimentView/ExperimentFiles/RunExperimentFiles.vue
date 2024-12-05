69c76bc73841a6e60c770236d76e02d1b58f1e9c
Alexander Peverett <Alexander.Peverett-CIC@ibm.com>
2024-12-05 11:02:31 +0000

    chore(RunExperimentFiles): update paragraph to reflect revised table functionality (#891)
    Signed-off-by: Alexander Peverett <Alexander.Peverett-CIC@ibm.com>

--- src/components/ExperimentView/ExperimentFiles/RunExperimentFiles.vue
+++ src/components/ExperimentView/ExperimentFiles/RunExperimentFiles.vue
@@ -2,9 +2,8 @@
   <h4>Provide Files</h4>
   <p><strong>Use the table to provide files to the workflow</strong></p>
   <p>
-    Click on the rows to disclose additional information for each file.
-    Configure and edit files in the disclosed section or use the three-dot menu
-    on the right. You must upload input files, while the others are optional.
+    Configure and edit files using the configure file action. You must upload
+    input files, while the others are optional.
   </p>
 </template>
 
